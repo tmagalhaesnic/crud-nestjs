a34ec4f8103fea4e9d77dbcc05b46764e093f166
thiagonic <tmagalhaes@nic.br>
2024-09-24 14:23:22 -0300

    crud e banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+  import { Module } from '@nestjs/common';
+  import { ConfigModule } from '@nestjs/config';
+  import { TypeOrmModule } from '@nestjs/typeorm';
+  import { UsersController } from './users/users.controller';
+  import { UsersService } from './users/users.service';
+  import { User } from './users/entities/user.entity';
 
-@Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+  @Module({
+    imports: [
+      ConfigModule.forRoot({isGlobal: true,}),
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT, 10),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [User],
+        synchronize: true,
+      }),
+      TypeOrmModule.forFeature([User]),
+    ],
+    controllers: [UsersController],
+    providers: [UsersService],
+  })
+  export class AppModule {}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,14 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+
+@Entity()
+@Unique(['email'])
+export class User {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+}
